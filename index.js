4fad6d96ce7e671fcf57bf46adc60db96e9b8900
Sheva618 <applesheva618@gmail.com>
2022-06-12 15:10:01 +0300

    blackboard

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const getShuffledArray = (num) => {
-  let number = num - 1;
-  let arr = [];
-  for (let index = 0; index < num; index = index + 2) {
-    arr[index] = number;
-    number = number + 1;
-    arr[index + 1] = number;
-    number = number - 3;
+export const createCalculator = () => {
+  let memory = 0;
+
+  function add(number) {
+    memory += number;
   }
-  return arr;
+
+  function decrease(number) {
+    memory -= number;
+  }
+
+  function reset() {
+    memory = 0;
+  }
+
+  function getMemo() {
+    return memory;
+  }
+
+  return {
+    add,
+    decrease,
+    reset,
+    getMemo,
+  };
 };
-
-console.log(getShuffledArray(20));
-
-[19, 20, 17, 18, 15, 16, 13, 14, 11, 12, 9, 10, 7, 8, 5, 6, 3, 4, 1, 2];
