656054cfc96b47abcdd755f7267f16d5dd3988b6
Sheva618 <applesheva618@gmail.com>
2022-06-19 13:55:55 +0300

    sandbox

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const numberOfPairs = (array) => {
-  let pairsCount = 0;
-  Object.values(
-    array.reduce(
-      (acc, color) => ({ ...acc, [color]: (acc[color] || 0) + 1 }),
-      {}
-    )
-  ).forEach((element) => {
-    if (element % 2 === 0) {
-      pairsCount += element / 2;
-    }
-  });
-  return pairsCount;
-};
+// function moveZeros(arr) {
+//   let zerosCounter = 0;
+//   return arr
+//     .filter((element) => {
+//       if (element === 0) {
+//         zerosCounter += 1;
+//       }
+//       return element !== 0;
+//     })
+//     .concat(Array.from({ length: zerosCounter }, (_, index) => 0));
+// }
+
+// console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
